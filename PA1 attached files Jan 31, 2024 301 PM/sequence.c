4688ec151d5f5528ecd3ae21b954a9f734345e5f
Katis009 <hkatis@purdue.edu>
2024-02-01 17:45:19 -0500

    start sequence

--- PA1 attached files Jan 31, 2024 301 PM/sequence.c
+++ PA1 attached files Jan 31, 2024 301 PM/sequence.c
@@ -1,6 +1,17 @@
 long *Generate_2p3q_Seq(int n, int *seq_size){
 
+    int q = 0;
+    int p = 0;
+    while (3^q < n){
+        q++;
+    }
+    int qStar = q - 1;
+    while (2^p < n){
+        p++;
+    }
+    int pStar = p - 1;
+    int maxSize = (((qStar + 2)*(qStar + 1))/2)+((pStar - qStar + 1)*(pStar - qStar));
+    int array[maxSize];
 
 
-    
 }
