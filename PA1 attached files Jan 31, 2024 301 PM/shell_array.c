5e302981b780494ed03fab2b7ffa2522497fa144
Katis009 <hkatis@purdue.edu>
2024-02-01 19:44:25 -0500

    1st shell array func

---
+++ PA1 attached files Jan 31, 2024 301 PM/shell_array.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+long *Array_Load_From_File(char *filename, int *size){
+    *size = (sizeof(filename)/sizeof(long));
+    long* array;
+    array = (long*)malloc(*size);
+    if (array == NULL){
+        int *size = 0;
+        return NULL;
+    }
+    FILE *fptr;
+    fptr = fopen(*filename, "r");
+    if (fptr == NULL){
+        int *size = 0;
+        return NULL;
+    }
+    //long mark;
+    fread(&array, sizeof(long), *size, fptr);
+    fclose(fptr);
+
+
+
+}
+
+int Array_Save_To_File(char *filename, long *array, int size);
+
+void Array_Shellsort(long *array, int size, long *n_comp);
